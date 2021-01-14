7c717e5dd9c855c22bbff120f26e8f7b9bd0f467
Khonsu Development <khonsu.development@gmail.com>
2021-01-14 16:21:05 +0100

    Make the image in CardView4 modifiable

--- materialcards/src/main/java/eu/khonsu/libraries/materialcards/CardView4.kt
+++ materialcards/src/main/java/eu/khonsu/libraries/materialcards/CardView4.kt
@@ -1,10 +1,13 @@
 package eu.khonsu.libraries.materialcards
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.DrawableRes
 import androidx.cardview.widget.CardView
 import eu.khonsu.libraries.materialcards.extensions.hideIf
 
@@ -16,6 +19,7 @@ import eu.khonsu.libraries.materialcards.extensions.hideIf
 @Suppress("MemberVisibilityCanBePrivate", "unused")
 class CardView4(context: Context, attrs: AttributeSet) : CardView(context, attrs) {
 
+    var image: ImageView? = null
     var title: TextView? = null
     var subtitle: TextView? = null
     var supportingText: TextView? = null
@@ -34,6 +38,9 @@ class CardView4(context: Context, attrs: AttributeSet) : CardView(context, attrs
         val typedArray = context.obtainStyledAttributes(attrs, R.styleable.CardView4)
         for (index in 0 until typedArray.indexCount) {
             when (val attr = typedArray.getIndex(index)) {
+                R.styleable.CardView4_cv4_srcImage -> setImageSrc(
+                    typedArray.getResourceId(attr, R.drawable.background_image)
+                )
                 R.styleable.CardView4_cv4_title -> setTitleText(typedArray.getString(attr))
                 R.styleable.CardView4_cv4_subtitle -> setSubtitleText(typedArray.getString(attr))
                 R.styleable.CardView4_cv4_supportingText -> setSupportingText(
@@ -54,12 +61,19 @@ class CardView4(context: Context, attrs: AttributeSet) : CardView(context, attrs
     }
 
     private fun initComponents() {
+        image = findViewById(R.id.image)
         title = findViewById(R.id.title)
         subtitle = findViewById(R.id.subtitle)
         supportingText = findViewById(R.id.supporting_text)
         actionButton1 = findViewById(R.id.action_button_1)
         actionButton2 = findViewById(R.id.action_button_2)
         actionButton3 = findViewById(R.id.action_button_3)
+    }
+
+    fun getImageSrc(): Drawable? = image?.drawable
+
+    fun setImageSrc(@DrawableRes value: Int) {
+        image?.setImageResource(value)
     }
 
     fun getTitleText(): CharSequence? = title?.text
