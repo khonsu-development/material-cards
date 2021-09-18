d5c57df801bd0e2018de2ad5fe402e05918f5318
Johann Roux <johann.ej.roux@gmail.com>
2021-09-18 19:52:40 +0200

    Convert Gradle files to the Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle.kts files
+    }
+}
+
+allprojects {
+    repositories {
+        mavenCentral()
+        google()
+        maven(url = "https://jitpack.io")
+    }
+}
+
+tasks.register<Delete>("clean").configure {
+    delete(rootProject.buildDir)
+}
+
+val materialVersion = "1.2.1"
---
+++ materialcards/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
+}
+
+group = "eu.khonsu.libraries"
+version = "1.0"
+
+android {
+    compileSdk = 30
+    buildToolsVersion = "30.0.3"
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 30
+        vectorDrawables.useSupportLibrary = true
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+    all {
+        exclude("httpclient")
+        exclude("commons-logging")
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.6.0")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+
+    // Design elements
+    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+    implementation("com.google.android.material:material:1.4.0")
+}
