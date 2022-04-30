667262955347aa01d0ce86e6d0c03f3202d27906
Johann Roux <johann.ej.roux@gmail.com>
2022-04-30 12:01:00 +0200

    Add maven-publish Gradle plugin for Jitpack

--- materialcards/build.gradle.kts
+++ materialcards/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.library")
     id("kotlin-android")
     id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
+    id("maven-publish")
 }
 
 group = "eu.khonsu.libraries"
