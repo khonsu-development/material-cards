48e7d9d5fae8e60b125428e6084596ef291b49bc
Johann Roux <johann.ej.roux@gmail.com>
2024-01-26 00:26:12 +0100

    Bump compileSdk to 34

--- materialcards/build.gradle.kts
+++ materialcards/build.gradle.kts
@@ -9,7 +9,7 @@ group = "eu.khonsu.libraries"
 version = "1.1-SNAPSHOT"
 
 android {
-    compileSdk = 31
+    compileSdk = 34
     namespace = "eu.khonsu.libraries.materialcards"
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
