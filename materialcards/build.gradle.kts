68b94fb300c7bdd91f14b60a640780f58aa3effa
johannroux <johann.ej.roux@gmail.com>
2025-09-03 10:20:44 +0200

    Use org.jetbrains.kotlin.android instead of kotlin-android

--- materialcards/build.gradle.kts
+++ materialcards/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.library")
-    id("kotlin-android")
+    id("org.jetbrains.kotlin.android") version "2.2.10"
     id("org.jlleitschuh.gradle.ktlint") version "13.1.0"
     id("maven-publish")
 }
