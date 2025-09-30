1e07efed4f21faf5e906d2cf80231c0762d28b09
johannroux <johann.ej.roux@gmail.com>
2025-09-30 13:55:58 +0200

    Upgrade miscellaneous dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.11.1")
+        classpath("com.android.tools.build:gradle:8.13.0")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
     }
@@ -22,5 +22,3 @@ allprojects {
 tasks.register<Delete>("clean").configure {
     delete(getLayout().buildDirectory)
 }
-
-val materialVersion = "1.2.1"
