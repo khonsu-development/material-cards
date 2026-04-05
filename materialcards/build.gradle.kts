09f48eb7465bdafeabbe6772243a930a9b86a87e
johannroux <johann.ej.roux@gmail.com>
2026-04-05 23:54:05 +0200

    build: Bump org.jetbrains.kotlin.android from 2.3.10 to 2.3.20

--- materialcards/build.gradle.kts
+++ materialcards/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.library")
-    id("org.jetbrains.kotlin.android") version "2.3.10"
+    id("org.jetbrains.kotlin.android") version "2.3.20"
     id("org.jlleitschuh.gradle.ktlint") version "14.2.0"
     id("maven-publish")
 }
